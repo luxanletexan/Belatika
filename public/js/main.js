c78438f1649399f21ca81dabef68382cb40ba1af
Asura <patxi064@gmail.com>
2019-02-03 15:09:03 +0100

    Redimensionnement du header

--- public/js/main.js
+++ public/js/main.js
@@ -19,21 +19,8 @@ $(function () {
         moveSpot(navElt, {x: 0, y: 0});
     });
 
-    /*$(window).scroll(function () {
-        moveSidenav();
-    });
-
-    moveSidenav();*/
-
     function moveSpot(domElt, coord) {
         domElt.style.backgroundPosition = 'calc(' + coord.x + 'px + 100%) calc(' + coord.y + 'px + 100%)';
-    }
-
-    function moveSidenav(){
-        let scroll = $(document).scrollTop();
-        let $belatikaNav = $('#belatika-nav');
-        let scrollMax = $('.belatika-content').height() - $belatikaNav.height();
-        $belatikaNav.css('margin-top', Math.min(scroll, scrollMax) + 'px');
     }
 });
 
