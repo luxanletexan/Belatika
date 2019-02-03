6f9bca89069b2717309d780fafaf01a2c56f49f7
Asura <patxi064@gmail.com>
2019-02-03 12:13:56 +0100

    Modification du menu de navigation

--- public/js/main.js
+++ public/js/main.js
@@ -19,11 +19,11 @@ $(function () {
         moveSpot(navElt, {x: 0, y: 0});
     });
 
-    $(window).scroll(function () {
+    /*$(window).scroll(function () {
         moveSidenav();
     });
 
-    moveSidenav();
+    moveSidenav();*/
 
     function moveSpot(domElt, coord) {
         domElt.style.backgroundPosition = 'calc(' + coord.x + 'px + 100%) calc(' + coord.y + 'px + 100%)';
