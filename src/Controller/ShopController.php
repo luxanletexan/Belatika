8010374ebe613523850038a5df352313c7028447
Zazia064 <zazia064@gmail.com>
2019-02-04 14:17:04 +0100

    conditions générales

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -15,12 +15,39 @@ class ShopController extends AbstractController
     /**
      * @Route("/", name="app_shop_index")
      */
-    public function index():Response
+    public function index(): Response
     {
         $items = $this->getDoctrine()->getRepository(Item::class)->findAllWithImages();
 
         $categories = $this->getDoctrine()->getRepository(Category::class)->findBy([], ['name' => 'ASC']);
 
         return $this->render('shop/index.html.twig', ['items' => $items, 'categories' => $categories]);
+    }
+
+    /**
+     * @Route("/conditions-generales-de-vente", name="app_shop_cgv")
+     */
+    public function cgv(): Response
+    {
+        $categories = $this->getDoctrine()->getRepository(Category::class)->findBy([], ['name' => 'ASC']);
+        return $this->render('shop/cgv.html.twig', ['categories' => $categories]);
+    }
+
+    /**
+     * @Route("/mentions-legales", name="app_shop_legals")
+     */
+    public function legals(): Response
+    {
+        $categories = $this->getDoctrine()->getRepository(Category::class)->findBy([], ['name' => 'ASC']);
+        return $this->render('shop/legals.html.twig', ['categories' => $categories]);
+    }
+
+    /**
+     * @Route("/condition-d-utilisation-du-blog", name="app_shop_bloginfos")
+     */
+    public function bloginfos(): Response
+    {
+        $categories = $this->getDoctrine()->getRepository(Category::class)->findBy([], ['name' => 'ASC']);
+        return $this->render('shop/bloginfos.html.twig', ['categories' => $categories]);
     }
 }
