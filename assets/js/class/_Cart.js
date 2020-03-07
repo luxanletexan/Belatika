0bc6f850da417b0a32409368758c3fdb209b9da8
Patxi <patxi064@gmail.com>
2020-03-07 17:32:45 +0100

    Panier AJAX -> prise en compte soldes

--- assets/js/class/_Cart.js
+++ assets/js/class/_Cart.js
@@ -6,6 +6,7 @@ export default class Cart {
      * @param {string} [options.button_class=cart-button] Classe CSS des boutons d'ajout au panier
      * @param {string} [options.shopping_bag_class=cart-hover] Classe CSS du menu panier
      * @param {string} [options.shopping_bag_item_class=cart-hover__item] Classe CSS des items du menu panier
+     * @param {string} [options.cart_quantity_class=navbar__item--cart] Classe CSS quantité dans le panier
      */
     constructor(options = {})
     {
@@ -14,13 +15,16 @@ export default class Cart {
             button_class: 'cart-button',
             shopping_bag_class: 'cart-hover',
             shopping_bag_item_class: 'cart-hover__item',
+            cart_quantity_class: 'navbar__item--cart',
         }, options);
         this.processing = false;
         this.cartContent = {};
+        this.onSales = false;
 
         //DOM handling
         this.buttons = [].slice.call(document.getElementsByClassName(this.options.button_class));
         this.shoppingBag = document.querySelector('.'+this.options.shopping_bag_class);
+        this.shoppingBagQuantity = document.querySelector('.'+this.options.cart_quantity_class);
         this.shoppingBagItemButtons = [].slice.call(document.querySelectorAll('.'+this.options.shopping_bag_item_class+' .remove-item'));
 
         //Events
@@ -45,6 +49,7 @@ export default class Cart {
         ajax(url, (response) => {
             spinner.classList.add('hidden');
             this.setButtonMode(button, added);
+            this.onSales = response.onSales === true;
             this.updateCart(response.cart);
             this.processing = false;
         });
@@ -67,10 +72,11 @@ export default class Cart {
             button.classList.remove('fa-spinner');
             button.classList.add('far');
             button.classList.add('fa-window-close');
-            this.updateCart(response.cart);
             if (addToCartButton instanceof HTMLElement) {
                 this.setButtonMode(addToCartButton, true);
             }
+            this.onSales = response.onSales === true;
+            this.updateCart(response.cart);
             this.processing = false;
         });
     }
@@ -90,14 +96,26 @@ export default class Cart {
         this.shoppingBag.innerHTML = '';
 
         let count = 0;
+        let total = 0;
         Object.values(this.cartContent).forEach(item => {
             let itemListElt = this.createShoppingBagItem(item);
+            let price = this.onSales ? item.discountPrice : item.price;
+            let quantity = parseInt(item.quantity);
+            total += quantity * parseFloat(price);
             this.shoppingBag.appendChild(itemListElt);
             count += item.quantity;
         });
-        this.shoppingBag.dataset.quantity = count;
+        if (total > 0) {
+            let shoppingBagTotal = this.createShoppingBagTotal(total);
+            this.shoppingBag.appendChild(shoppingBagTotal);
+        }
+        this.shoppingBagQuantity.dataset.quantity = count;
     }
 
+    /**
+     * @param {Object} item
+     * @return HTMLElement
+     */
     createShoppingBagItem(item)
     {
         let itemContainer = this.createElementWithClasses('div', this.options.shopping_bag_item_class);
@@ -108,9 +126,10 @@ export default class Cart {
         itemImage.src = item.images[0].cachePath;
         itemImage.alt = item.images[0].alt;
         let itemPrice = this.createElementWithClasses('span', this.options.shopping_bag_item_class+'__price');
-        itemPrice.innerText = new Intl.NumberFormat('fr-FR', {style: 'currency', currency: 'EUR'}).format(item.price);
+        let price = this.onSales ? item.discountPrice : item.price;
+        itemPrice.innerText = new Intl.NumberFormat('fr-FR', {style: 'currency', currency: 'EUR'}).format(price);
         let itemRemove = this.createElementWithClasses('i', ['remove-item', 'far', 'fa-window-close']);
-        itemRemove.setAttribute('item_id', item.id);
+        itemRemove.setAttribute('data-item_id', item.id);
         itemRemove.setAttribute('data-remove_item_url', item.removeLink);
         itemRemove.addEventListener('click', this.onShoppingBagButtonClick.bind(this, itemRemove));
 
@@ -122,6 +141,27 @@ export default class Cart {
         return itemContainer;
     }
 
+    /**
+     * @param {float} total
+     * @return HTMLElement
+     */
+    createShoppingBagTotal(total)
+    {
+        let totalContainer = this.createElementWithClasses('div', this.options.shopping_bag_class+'__total');
+        let totalHeading = this.createElementWithClasses('span');
+        totalHeading.innerText = 'Total:';
+        let totalAmount = this.createElementWithClasses('span', this.options.shopping_bag_class+'__total__amount');
+        totalAmount.innerText = new Intl.NumberFormat('fr-FR', {style: 'currency', currency: 'EUR'}).format(total);
+        totalContainer.appendChild(totalHeading);
+        totalContainer.appendChild(totalAmount);
+        return totalContainer;
+    }
+
+    /**
+     * @param {string} tag
+     * @param {string|array} classes
+     * @return HTMLElement
+     */
     createElementWithClasses(tag, classes = [])
     {
         if (!Array.isArray(classes)) {
