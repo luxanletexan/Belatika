eedc51589d6728e4dc10bc5a3d5223746d42af38
Patxi <patxi064@gmail.com>
2020-04-05 16:50:09 +0200

    Stats visites

---
+++ src/Controller/StatController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/stats")
+ */
+class StatController extends ParentController
+{
+    //TODO: stats visites
+}
--- src/EventListener/RequestListener.php
+++ src/EventListener/RequestListener.php
@@ -4,14 +4,47 @@
 namespace App\EventListener;
 
 
+use App\Entity\StatVisit;
+use Doctrine\ORM\EntityManager;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
 class RequestListener
 {
+    /**
+     * @var EntityManager
+     */
+    private $manager;
+
+    public function __construct(EntityManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
     public function onKernelRequest(GetResponseEvent $event)
     {
         $session = $event->getRequest()->getSession();
         $request = $event->getRequest();
+
+        //Stats visite
+        $referer = $request->headers->get('referer');
+        $host = $request->getHost();
+        if (preg_match("#$host#", $referer) !== 1) {
+            $clientIpMd5 = md5($request->getClientIp());
+            $requestedURI = $request->getRequestUri();
+
+            $statVisit = $this->manager->getRepository(StatVisit::class)->findOneBy(['ip_md5' => $clientIpMd5]);
+            if ($statVisit instanceof StatVisit) {
+                $statVisit
+                    ->addReferer($referer)
+                    ->addRequestedUri($requestedURI);
+            } else {
+                $statVisit = new StatVisit();
+                $statVisit->setIpMd5($clientIpMd5)->setReferers([$referer])->setRequestedUris([$requestedURI]);
+            }
+
+            $this->manager->persist($statVisit);
+            $this->manager->flush();
+        }
 
         $route = $request->attributes->get('_route');
 
