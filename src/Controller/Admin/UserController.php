462bdf408cc4733dee1d80084222e2069f7744cf
Patxi <patxi064@gmail.com>
2020-08-27 19:21:55 +0200

    Suppression users inactifs

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -24,4 +24,22 @@ class UserController extends AdminController
 
         return $this->render($this->getTemplate('admin/user/index.html.twig'), ['users' => $users]);
     }
+
+    /**
+     * @Route("/clear-inactive")
+     * @return Response
+     */
+    public function clearInactive()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $users = $em->getRepository(User::class)->findBy(['enabled' => 0]);
+
+        foreach ($users as $user) {
+            $em->remove($user);
+        }
+        $em->flush();
+
+        return $this->redirectToRoute('app_admin_user_index');
+    }
 }
