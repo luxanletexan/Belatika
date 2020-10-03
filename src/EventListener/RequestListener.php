e3ae46b3a062a5135f09218a339442dafa966f58
Patxi <patxi064@gmail.com>
2020-10-03 09:47:09 +0200

    Retrait stats visite

--- src/EventListener/RequestListener.php
+++ src/EventListener/RequestListener.php
@@ -37,28 +37,6 @@ class RequestListener
             die();
         }
 
-        //Stats visite
-        $referer = $request->headers->get('referer');
-        $referer = empty($referer) ? 'none' : $referer;
-        $host = $request->getHost();
-        if (preg_match("#$host#", $referer) !== 1) {
-            $clientIpMd5 = md5($request->getClientIp());
-            $requestedURI = $request->getRequestUri();
-            $requestedURI = empty($requestedURI) ? 'none' : $requestedURI;
-            $statVisit = $this->manager->getRepository(StatVisit::class)->findOneBy(['ip_md5' => $clientIpMd5]);
-            if ($statVisit instanceof StatVisit) {
-                $statVisit
-                    ->addReferer($referer)
-                    ->addRequestedUri($requestedURI);
-            } else {
-                $statVisit = new StatVisit();
-                $statVisit->setIpMd5($clientIpMd5)->setReferers([$referer])->setRequestedUris([$requestedURI]);
-            }
-
-            $this->manager->persist($statVisit);
-            $this->manager->flush();
-        }
-
         $route = $request->attributes->get('_route');
 
         if (
